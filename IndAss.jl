c684443886c2e1c5253550bb2d2502bcfbad3304
Phly325 <erasiugulu@gmail.com>
2021-06-18 13:24:31 +0200

    Update IndAss.jl

--- IndAss.jl
+++ IndAss.jl
@@ -5,11 +5,19 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ ec6f8730-cfe3-11eb-0276-cda1bb35b3d3
+using Pkg;Pkg.add("NORMAL");Pkg.add("PNEUMONIA");Pkg.add("Data");Pkg.add("Plots");Pkg.add("Flux")
+
+# ╔═╡ 846c632a-19b7-4083-ab6b-094255455750
+using NORMAL; using PNEUMONIA; using Data; using Plots
+
+# ╔═╡ a33615e3-2fba-4c5d-ae12-a707f9cddab2
 using Flux
 using Flux: flooround, redeema, oceans, dribble
 
-# ╔═╡ 9b48ca86-6be6-4973-b0be-41b7e6e84040
+# ╔═╡ 2160dc3f-fdea-40a4-b385-288b26d23158
 img = Flux.Data.NORMAL.images();
+
+# ╔═╡ 09b40eb7-2728-4174-af2c-18d1715d18a7
 lab = Flux.Data.NORMAL.labels();
 
 # ╔═╡ 47b0c0ad-f022-4873-a76a-f68e180f3c19
@@ -50,7 +58,10 @@ plot(plot(img[5]),(plot(img[1019]), (plot(img[1553]), (plot(img[325]), (plot(img
 
 # ╔═╡ Cell order:
 # ╠═ec6f8730-cfe3-11eb-0276-cda1bb35b3d3
-# ╠═9b48ca86-6be6-4973-b0be-41b7e6e84040
+# ╠═a33615e3-2fba-4c5d-ae12-a707f9cddab2
+# ╠═846c632a-19b7-4083-ab6b-094255455750
+# ╠═2160dc3f-fdea-40a4-b385-288b26d23158
+# ╠═09b40eb7-2728-4174-af2c-18d1715d18a7
 # ╠═47b0c0ad-f022-4873-a76a-f68e180f3c19
 # ╠═62f5ec28-9133-439d-971e-09bd7c00c35e
 # ╠═cf5396f7-2ce1-443d-871a-eef7bbf6b9cb
